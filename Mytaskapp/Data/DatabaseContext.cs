49a5e9d97d62946321d293850403b496657821d1
Kishanmaisuria <99621357+Kishanmaisuria@users.noreply.github.com>
2023-11-13 15:47:40 +1300

    sumthing???

--- Mytaskapp/Data/DatabaseContext.cs
+++ Mytaskapp/Data/DatabaseContext.cs
@@ -1,4 +1,6 @@
-﻿
+﻿using System;
+using System.Runtime;
+using Mytaskapp.Models;
 using SQLite;
 
 namespace Mytaskapp.Data
@@ -9,6 +11,32 @@ namespace Mytaskapp.Data
         private static string Dbpath => Path.Combine(FileSystem.AppDataDirectory, DbName);
 
         private SQLiteAsyncConnection _connection;
-        private SQLiteAsyncConnection Database => (_connection ??= new SQLiteAsyncConnection(Dbpath, SQLiteOpenFlags.Create| SQLiteOpenFlags.ReadWrite|SQLiteOpenFlags.SharedCache));
+        private SQLiteAsyncConnection Database => (_connection ??= new SQLiteAsyncConnection(Dbpath, SQLiteOpenFlags.Create | SQLiteOpenFlags.ReadWrite | SQLiteOpenFlags.SharedCache));
+
+        
+
+
+        public async Task<IEnumerable<TTable>> GetAllAsync<TTable>() where TTable : class, new() 
+        {
+         await Database.CreateTableAsync<TTable>();
+         return await Database.Table<TTable>().ToListAsync();
+        
+        }
+
+        public async Task<IEnumerable<TTable>> GetFileteredAsync<TTable>(Exception<Func<TTable, bool>> predicate) where TTable : class, new()
+        {
+            await Database.CreateTableAsync<TTable>();
+            return await Database.Table<TTable>().Where(predicate).ToListAsync();
+
+        }
+
+
+
+        //public async Task Try()
+        //{
+        //    await Database.CreateTableAsync<Product>();
+        //    Database.Table<Product>().ToListAsync();
+
+        //}
     }
 }
