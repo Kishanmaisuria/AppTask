4cf5c5b82818384255d59774c2c4560bf214dce2
Kishanmaisuria <99621357+Kishanmaisuria@users.noreply.github.com>
2023-11-13 16:29:37 +1300

    hamburger

--- Mytaskapp/Data/DatabaseContext.cs
+++ Mytaskapp/Data/DatabaseContext.cs
@@ -1,7 +1,7 @@
-﻿using System;
-using System.Runtime;
-using Mytaskapp.Models;
+﻿
+using CoreGraphics;
 using SQLite;
+using System.Linq.Expressions;
 
 namespace Mytaskapp.Data
 {
@@ -14,21 +14,33 @@ namespace Mytaskapp.Data
         private SQLiteAsyncConnection Database => (_connection ??= new SQLiteAsyncConnection(Dbpath, SQLiteOpenFlags.Create | SQLiteOpenFlags.ReadWrite | SQLiteOpenFlags.SharedCache));
 
         
+        // Create table if not exists = CTINE
+        private async Task CTINE<TTable>() where TTable : class, new() 
+        {
+            await Database.CreateTableAsync<TTable>();
+        }
+
+        private async Task<AsyncTableQuery<TTable>> GetTableAsync<TTable>() where TTable : class, new()
+        {
+            await CTINE<TTable>();
+            return Database.Table<TTable>();
+        }
+
 
 
         public async Task<IEnumerable<TTable>> GetAllAsync<TTable>() where TTable : class, new() 
         {
-         await Database.CreateTableAsync<TTable>();
-         return await Database.Table<TTable>().ToListAsync();
+            var table = await GetTableAsync<TTable>();
+            return await table.ToListAsync();
         
         }
 
-        public async Task<IEnumerable<TTable>> GetFileteredAsync<TTable>(Exception<Func<TTable, bool>> predicate) where TTable : class, new()
+        public async Task<IEnumerable<TTable>> GetFileteredAsync<TTable>(Expression<Func<TTable, bool>> predicate) where TTable : class, new()
         {
             await Database.CreateTableAsync<TTable>();
             return await Database.Table<TTable>().Where(predicate).ToListAsync();
-
         }
+
 
 
 
